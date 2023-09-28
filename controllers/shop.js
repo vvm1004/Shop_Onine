cef97f3763dd15d3ae7ee6941266c0f28c38d36f
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-09-28 23:05:23 +0700

    Fix footer

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const PDFDocument = require('pdfkit');
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
   const page = +req.query.page || 1;
